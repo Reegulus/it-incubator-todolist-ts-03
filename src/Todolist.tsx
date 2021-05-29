f72b02e0ee31fa7a260e9ea369ed687b8a97073e
Egorych <mikhalevichegor@gmail.com>
2021-05-29 22:07:17 +0300

    очищать инпут после добавления таски

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -20,26 +20,41 @@ export function Todolist(props: PropsType) {
     return <div>
         <h3>{props.title}</h3>
         <div>
-            <input value={title} onChange={ (e) => {setTitle(e.currentTarget.value)}}/>
-            <button onClick={ () => {props.addTask(title)}}>+</button>
+            <input value={title} onChange={(e) => {
+                setTitle(e.currentTarget.value)
+            }}/>
+            <button onClick={() => {
+                props.addTask(title)
+                setTitle("")
+            }}>+
+            </button>
         </div>
         <ul>
             {
                 props.tasks.map(t => <li key={t.id}>
                     <input type="checkbox" checked={t.isDone}/>
                     <span>{t.title}</span>
-                    <button onClick={ () => { props.removeTask(t.id) } }>x</button>
+                    <button onClick={() => {
+                        props.removeTask(t.id)
+                    }}>x
+                    </button>
                 </li>)
             }
         </ul>
         <div>
-            <button onClick={ () => { props.changeFilter("all") } }>
+            <button onClick={() => {
+                props.changeFilter("all")
+            }}>
                 All
             </button>
-            <button onClick={ () => { props.changeFilter("active") } }>
+            <button onClick={() => {
+                props.changeFilter("active")
+            }}>
                 Active
             </button>
-            <button onClick={ () => { props.changeFilter("completed") } }>
+            <button onClick={() => {
+                props.changeFilter("completed")
+            }}>
                 Completed
             </button>
         </div>
